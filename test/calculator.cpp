5e9e9a9121f11df5c769d36b319f2b3ac45ade50
jamieb-dev <jamieb2684@gmail.com>
2024-09-22 20:43:40 +0100

    Removed use of unnecessary stoi() function

---
+++ test/calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+
+
+
+int main(void){
+    int input;
+    std::cin >> input;
+    std::cout << input;
+    std::cout << std::stoi(input);
+    
+return 0;
+}
+
